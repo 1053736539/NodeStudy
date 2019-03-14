42674b99e16785eb37ab813dce9b25cefb300c30
刁迎冬 <1053736539@qq.com>
2019-03-14 18:15:09 +0800

    Node.js单线程多线程性能比较

--- nodeOne2.js
+++ nodeOne2.js
@@ -1,7 +1,8 @@
 
 var http = require('http');
-
+// HTTP创建服务器用的
 var fs=require("fs");
+// 读取文件用的
 
 var server=http.createServer(function (req,res) {
     // 通过调取remoteAddress方法来获取用户IP
@@ -16,6 +17,12 @@ var server=http.createServer(function (req,res) {
 server.listen(8888);
 console.log("服务器已经运行在8888端口");
 
+
+
+// Node.js适合开发I/O多的业务，而不适合计算任务繁重的业务
+//
+// 在多线程程序中例如PHP,CPU经常会等待I/O结束，但是CPU还是处于空闲状态，导致性能白白消耗。
+// 在单线程中，当访问并行极大的时候，CPU理论上能发挥100%的性能
 
 //
 // http.createServer(function (request, response) {
